2dd7aa1095fe0e424da230de965dde1d0d07b0aa
szlanny <szlanny@gmail.com>
2015-11-08 04:31:06 +0800

    add spider middleware

--- app.go
+++ app.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	log "github.com/Sirupsen/logrus"
+	"github.com/wcreate/home/tools"
 	"github.com/wcreate/tkits"
 	"github.com/wcreate/wuc"
 	"gopkg.in/macaron.v1"
@@ -12,6 +13,17 @@ func main() {
 	log.Debug("Starting server...")
 	tkits.SyncDB()
 
+	m := macaron.Classic()
+	m.Use(macaron.Renderer())
+	m.Use(tools.SpiderFunc())
+
+	// user centre
+	wuc.InitHandles(m)
+
+	run(m)
+}
+
+func run(m *macaron.Macaron) {
 	cfg := macaron.Config()
 	web, err := cfg.GetSection("web")
 	if err != nil {
@@ -19,11 +31,5 @@ func main() {
 	}
 	ip := web.Key("ip").MustString("0.0.0.0")
 	port := web.Key("port").MustInt(8080)
-
-	m := macaron.Classic()
-	m.Use(macaron.Renderer())
-	// user
-	wuc.InitHandles(m)
-
 	m.Run(ip, port)
 }
